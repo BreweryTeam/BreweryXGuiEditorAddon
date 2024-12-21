e3ab2b869382c0d9206b4a6adeb8494b087a1427
Jsinco <jonahmarksinclair@gmail.com>
2024-12-21 04:48:48 -0500

    update gradle, couple other things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+
 plugins {
     id("java")
     kotlin("jvm")
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
+    id("com.modrinth.minotaur") version "2.8.7"
 }
 
 group = "dev.jsinco.brewery"
@@ -15,7 +19,6 @@ repositories {
 dependencies {
     compileOnly("com.dre.brewery:BreweryX:3.4.3-SNAPSHOT")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    compileOnly(kotlin("stdlib-jdk8"))
 }
 
 java {
@@ -23,5 +26,39 @@ java {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(project.version.toString())
+        channel.set("Release")
+        id.set("BreweryGuiEditor")
+        apiKey.set(System.getenv("HANGAR_TOKEN"))
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+                platformVersions.set(listOf("1.20.x", "1.21.x"))
+            }
+        }
+        changelog.set(readChangeLog())
+    }
+}
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("BreweryGuiEditor") // This can be the project ID or the slug. Either will work!
+    versionNumber.set(project.version.toString())
+    versionType.set("release") // This is the default -- can also be `beta` or `alpha`
+    uploadFile.set(tasks.jar)
+    loaders.addAll("paper", "purpur", "folia")
+    gameVersions.addAll("1.20.x", "1.21.x")
+    changelog.set(readChangeLog())
+}
+
+fun readChangeLog(): String {
+    val text: String = System.getenv("CHANGELOG") ?: file("CHANGELOG.md").run {
+        if (exists()) readText() else "No Changelog found."
+    }
+    return text.replace("\${version}", project.version.toString())
 }
